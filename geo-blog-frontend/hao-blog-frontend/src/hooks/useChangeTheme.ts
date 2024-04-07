6cb017076ffde5086a00f96b47ae7d1d10574545
邸浩哲 <dihaozhe@outlook.com>
2024-04-07 09:14:37 +0800

    initial: GeoBlog 项目初始化

--- geo-blog-frontend/hao-blog-frontend/src/hooks/useChangeTheme.ts
+++
@@ -1,7 +0,0 @@
-import { useSystemStore } from '@/stores/system'
-
-export const useChangeTheme = (theme: string) => {
-  const systemStore = useSystemStore()
-  document.getElementsByTagName('html')[0].dataset.theme = theme
-  systemStore.theme = theme
-}
