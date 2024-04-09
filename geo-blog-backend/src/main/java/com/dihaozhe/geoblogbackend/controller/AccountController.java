497c733820b220225b3fb0737cc052d053fab571
邸浩哲 <dihaozhe@outlook.com>
2024-04-09 21:32:57 +0800

    initial: GeoBlog 项目初始化

--- geo-blog-backend/src/main/java/com/dihaozhe/geoblogbackend/controller/AccountController.java
+++ geo-blog-backend/src/main/java/com/dihaozhe/geoblogbackend/controller/AccountController.java
@@ -1,20 +1,23 @@
 package com.dihaozhe.geoblogbackend.controller;
 
+import com.dihaozhe.geoblogbackend.pojo.entity.Account;
 import com.dihaozhe.geoblogbackend.service.AccountService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/test")
-public class TestController {
+@RequestMapping("/api/account")
+public class AccountController {
 
     @Resource
     public AccountService accountService;
 
-    @GetMapping("/hello")
-    public String test() {
-        return "Hello World";
+    @GetMapping("/list")
+    public List<Account> list() {
+        return accountService.list();
     }
 }
